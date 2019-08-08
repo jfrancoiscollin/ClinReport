47c1e97bcd7a5271382067319a1e33616f0845f8
Jfrancoiscollin <jfcollin@live.fr>
2019-08-08 16:48:29 +0200

    just few change on vignette, passing the time...

--- inst/transpose.desc.R
+++ inst/transpose.desc.R
@@ -19,13 +19,22 @@ transpose=function(desc)
 		x1=desc$x1
 		x2=desc$x2
 		nbcol=desc$nbcol
+		type.desc= desc$type.desc
 		
-		
-	   m=melt(output,measure.vars=colnames(output)[-c(1:nbcol)],variable.name=x1)
+	   m=melt(output,measure.vars=colnames(output)[-c(1:nbcol)],
+			   variable.name=x1)
+	   
 	   if(!is.null(at.row)) m=m[m$value!="",]
-       if(!is.null(x2)) form=as.formula(paste0(x2,"+",x1,"~Statistics"))
-       if(is.null(x2)) form=as.formula(paste0(x1,"~Statistics"))
-	   output=dcast(m,form)
+	   
+	   if(type.desc=="quanti")
+	   {
+		   if(!is.null(x2)) form=as.formula(paste0(x2,"+",x1,"~Statistics"))
+		   if(is.null(x2)) form=as.formula(paste0(x1,"~Statistics")) 
+		   
+		   output=dcast(m,form)
+	   }
+
+	   
        if(!is.null(at.row)) output=spacetable(output,at.row=at.row)
 	   
 	   output
