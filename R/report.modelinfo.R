2a6c47461b0ba4a67d30ce009a17685cba81d018
Jfrancoiscollin <jfcollin@live.fr>
2019-08-12 14:23:37 +0200

    add lmer model to repot.model info

--- R/report.modelinfo.R
+++ R/report.modelinfo.R
@@ -5,7 +5,7 @@
 
 #' Export model information (still experimental)
 #'
-#' @param object A model (for now glm, lme and coxph models are availlable)
+#' @param object A model (for now glm, lme, lmer and coxph models are availlable)
 #' @param doc NULL or a rdocx object
 #' @param page.break Logical. If TRUE it adds a page break after the output. Default to TRUE
 #' @param ... Other arguments
@@ -29,15 +29,24 @@
 #'library(officer)
 #'library(flextable)
 #'library(nlme)
-#' 
+#'library(lme4)
 #' 
 #' data(datafake)
+#' 
+#' #Model info for lme model
 #' 
 #' mod=lme(y_numeric~GROUP+TIMEPOINT,random=~1|SUBJID,data=datafake,na.action=na.omit)
 #' 
 #' # Show in HTML (can be inserted in an R markdown or a MS Word document)
 #' 
 #' report.modelinfo(mod)
+#' 
+#' #Model info for lmer model
+#' 
+#' mod=lmer(y_numeric~GROUP+TIMEPOINT+(1|SUBJID),data=datafake,na.action=na.omit)
+#' 
+#' report.modelinfo(mod)
+#' 
 #' 
 #' @rdname report.modelinfo
 #' @export
@@ -70,6 +79,53 @@ report.modelinfo.lme=function(object,doc=NULL,page.break=TRUE,...)
 	nbgrp=paste0(dd$ngrps[1:dd$Q])
 	aic_bic=paste0("AIC =",round(summary(object)$AIC,3), "; BIC = ",round(summary(object)$BIC,3))
 	
+	
+	names=c("R package / function","Type of model","Model formula",
+			"Method of adjustment","NA handling",
+			"Number of Observations",
+			"Number of Groups",
+			"Quality of adjustment")
+	
+	model.info=data.frame(Information=names,Details=c(rpack_func,label,object.formula,
+					method,na.handling,nbobs,nbgrp,aic_bic))
+	
+	ft=flextable(model.info)
+	ft=bg(ft, i = 1, bg = "#DCDCDC", part = "header")
+	
+	ft = width(ft, j=1,width = 2)
+	ft = width(ft, j=2,width = 6)
+	
+	if (!is.null(doc)) {
+		if (class(doc) != "rdocx") 
+			stop("doc must be a rdocx object")
+		doc <- body_add_par(doc, "", style = "Normal")
+		doc <- body_add_flextable(doc, value = ft)
+		if (page.break) 
+			doc = body_add_break(doc)
+		return(doc)
+	}
+	else {
+		return(ft)
+	}
+	
+}
+
+
+#' @rdname report.modelinfo
+#' @export 
+
+report.modelinfo.lmerMod=function(object,doc=NULL,page.break=TRUE,...)
+{
+	
+	rpack_func="R Package lme4 , function lmer"
+	if(isLMM(object))	label="Linear Mixed Effect Model"	
+	object.formula=paste0(deparse(object@call),collapse="\n")
+	if(isREML(object)) method="REML"
+	if(!isREML(object)) method="ML"
+	nbobs=nobs(object)
+	nbgrp=ngrps(object)
+	aic_bic=paste0("AIC =",round(AIC(object),3), "; BIC = ",round(BIC(object),3))
+	na.handling=attr(na.action(object),"class")
 	
 	names=c("R package / function","Type of model","Model formula",
 			"Method of adjustment","NA handling",
--- inst/report.ae.R
+++ inst/report.ae.R
@@ -7,66 +7,34 @@ library(reshape2)
 library(ClinReport)
 
 
-data=read.csv2("C:\\Users\\jfcollin\\Google Drive\\ClinReport\\AE.csv")
-data=data[c("SUBJID","ABON_PV","SOCNAME","PTNAME")]
-
-data=droplevels(data[data$ABON_PV!="",])
-#head(data)
-
-# Construction of the AE table
-
-split.soc=list()
-for(i in 1:nrow(data))
-{
-	split.soc[[i]]=strsplit(as.character(data$SOCNAME[i]),"; ")[[1]]
-}
-names(split.soc)=data$SUBJID
-split.soc=melt(split.soc)
-colnames(split.soc)=c("SOCNAME","SUBJID")
-
-
-split.pt=list()
-for(i in 1:nrow(data))
-{
-	split.pt[[i]]=strsplit(as.character(data$PTNAME[i]),"; ")[[1]]
-}
-names(split.pt)=data$ABON_PV
-split.pt=melt(split.pt)
-colnames(split.pt)=c("PTNAME","ABON_PV")
-
-AE=droplevels(data.frame(cbind(split.soc,split.pt)))
-
-# check
-#AE[AE$SUBJID=="SP-HEJA-1-5905",]
-#head(AE)
-
-# Stat reporting at AE level
-
-stat=report.quali(data=AE,y="PTNAME",x1="ABON_PV",x2="SOCNAME",
-		x2.label="System Organ Class",
-		y.levels.label="Prefered Terms",
-		at.row="System Organ Class",remove.zero=T)
-
-
-report.doc(stat,title="Adverse Event reporting",
-		colspan.value="Pharmaco Vigilance Clissification")
-
-
-#TODO check why non missing in report.quali if remove.zero=T
-# do we display the 0(0%) values?
+load("C:\\Users\\jfcollin\\git\\data\\data.RData")
 
 # Calculation of at least one subject with an AE
 
 
 
+at.least.one=function(data,subjid,var)
+{
+
+	t=table(data[,subjid],data[,var])
+	
+	at_least_one=apply(t,2,function(x)
+			{
+				length(x[x!=0])
+			})
+	
+	
+	at_least_one
+}
 
 
 
 
+at.least.one(data=data,subjid="SUBJID",var="AE_niv1")
+at.least.one(data=data,subjid="SUBJID",var="AE_niv2")
 
 
-
-
+transpose(report.quali(data=data,y="AE_niv1"))
 
 
 
